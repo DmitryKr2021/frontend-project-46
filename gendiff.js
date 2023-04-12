74f23e7c68e89e9c192c7f7c789ad75d7410e19e
Dmitry <krdvmail@mail.ru>
2023-04-12 11:19:13 +0300

    modify file

--- gendiff.js
+++ gendiff.js
@@ -6,13 +6,12 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1')
-  .option('-V, --version', 'output the version number 12345')
+  //.option('-V, --version', 'output the version number 12345')
   .option('-h, --help', 'display help for command')
   .option('-f, --format <type>', 'output format')
-
-  .command('gendiff')
   .argument('<filepath1>', 'first configuration file')
   .argument('<filepath2>', 'second configuration file')
+  .command('gendiff')
   .action((filepath1, filepath2, options) => {
     console.log(filepath1)
   });
