215feca958da82593b4139250694baf82615b97b
Dmitry <krdvmail@mail.ru>
2023-04-14 10:29:03 +0300

    add readFileSync

--- gendiff.js
+++ gendiff.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
-import { readFileSync } from 'node:fs';
+import { readFileSync } from 'fs';
+import { path } from 'path';
 
 const program = new Command();
 
@@ -12,6 +13,10 @@ program
   .argument('<filepath1>', 'first configuration file')
   .argument('<filepath2>', 'second configuration file')
   .action((filepath1, filepath2, options) => {
-    console.log(filepath1)
+    const objFile1 = JSON.parse(readFileSync(`${filepath1}.json`));
+    console.log(objFile1);
+    console.log(path.resolve(filepath1));
+    //const objFile2 = JSON.parse(readFileSync(`${filepath2}.json`));
+    //console.log(objFile2);
   });
 program.parse(process.argv);
