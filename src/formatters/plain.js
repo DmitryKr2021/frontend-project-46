f11d3a725785ddaec5afff513acf50bb6c6c85e2
Dmitry <krdvmail@mail.ru>
2023-06-14 09:47:22 +0300

    refactoring 4.1

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -5,19 +5,18 @@ const stringify = (data) => {
 
 const plain = (dataDif) => {
   const inner = (data, path = '') => {
-    const res = data.map((item) => {
-      const pathPoint = path === '' ? '' : `${path}.`;
+    const pathPoint = path === '' ? '' : `${path}.`;
+    const result = data.map((item) => {
       switch (item.type) {
         case 'added': return `Property '${pathPoint}${item.key}' was added with value: ${stringify(item.value2)}`;
         case 'deleted': return `Property '${pathPoint}${item.key}' was removed`;
         case 'changed': return `Property '${pathPoint}${item.key}' was updated. From ${stringify(item.value1)} to ${stringify(item.value2)}`;
-        case 'unchanged': return null;
         case 'nested': { const newPath = `${pathPoint}${item.key}`;
           return `${inner(item.children, newPath)}`; }
-        default: break;
+        default: return null;
       }
     }, '');
-    return res.filter((item) => item).join('\n');
+    return result.filter((item) => item).join('\n');
   };
   return inner(dataDif).trim();
 };
