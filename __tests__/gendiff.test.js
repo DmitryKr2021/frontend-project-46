0f526e75bcbb16e8aab121ca0654b55aea98345b
Dmitry <krdvmail@mail.ru>
2023-06-16 16:23:37 +0300

    refactoring 4.11

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -3,11 +3,14 @@ import path from 'path';
 import fs from 'fs';
 import { fileURLToPath } from 'url';
 import genDiff from '../src/formatters/index.js';
+import getDiff from '../src/getdiff.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
+const jsonDif = getDiff(getFixturePath('file1.json'), getFixturePath('file2.json'));
+const ymlDif = getDiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'));
 const resultExpected = (fileName) => fs.readFileSync(getFixturePath(fileName), 'utf-8').trim();
 
 test.each([
@@ -32,6 +35,8 @@ test.each([
 ])('compare', ({
   format, expected,
 }) => {
-  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), format)).toBe(expected);
-  expect(genDiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'), format)).toBe(expected);
+  expect(genDiff(jsonDif, format)).toBe(expected);
+  expect(genDiff(ymlDif, format)).toBe(expected);
+  /* expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), format)).toBe(expected);
+  expect(genDiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'), format)).toBe(expected); */
 });
--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,12 +1,21 @@
 import stylish from './stylish.js';
 import plain from './plain.js';
-import getDiff from '../getdiff.js';
+// import getDiff from '../getdiff.js';
 
-const genDiff = (filepath1, filepath2, formatName) => {
+/*const genDiff = (filepath1, filepath2, formatName) => {
   switch (formatName) {
     case 'plain': return (plain(getDiff(filepath1, filepath2)));
     case 'json': return (JSON.stringify(getDiff(filepath1, filepath2)));
     default: return (stylish(getDiff(filepath1, filepath2)));
   }
+};*/
+
+const genDiff = (filesDif, formatName) => {
+  switch (formatName) {
+    case 'plain': return (plain(filesDif));
+    case 'json': return (JSON.stringify(filesDif));
+    default: return (stylish(filesDif));
+  }
 };
+
 export default genDiff;
